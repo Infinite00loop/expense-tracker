cd7d3587599e8ff97d70735b4e234f4ea192693e
Infinite00loop <samriddhisingh07092002@gmail.com>
2023-10-22 17:40:13 +0530

    demo

---
+++ practice.js
@@ -0,0 +1,48 @@
+const post = [{title: 'POST'}];
+var active;
+var interval;
+function getPost() {
+    clearInterval(interval);
+    interval = setTimeout( () => {
+        console.group(post);
+        console.log(active);
+        
+    }, 1000);
+
+}
+    function createPost() {
+        return new Promise( (resolve, reject) => {
+            setTimeout( () => {
+                post.push({title: 'POST'});
+                
+                resolve();
+            }, 1000)
+        }) 
+    }
+    function deletePost(){
+        return new Promise((resolve, reject) => {
+            setTimeout( () => {
+                if(post.length > 0){
+                    const poppedElement  = post.pop();
+                    resolve(poppedElement);
+                } else {
+                    reject("ERROR: ARRAY IS EMPTY")
+                }
+            }, 1000)
+        })
+    }
+    function updateLastUserActivityTime() {
+        return new Promise( (resolve, reject) => {
+            setTimeout( () => {
+                active= new Date();
+                resolve(active);
+            }, 1000)
+        }) 
+    }
+   Promise.all([createPost(),createPost(),createPost(),createPost(),updateLastUserActivityTime()]).then(
+    ()=>{
+        getPost();
+        deletePost().then(getPost());
+
+    }
+   )
